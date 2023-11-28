1d0cacaa2e9a813d80b3070a341c7b7697bf243f
Nabil faturrahman <nabilfaturr44@gmail.com>
2023-11-28 16:13:03 +0700

    ready to live

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
+import path from "path";
+
+const __dirname = path.resolve();
 
 mongoose
   .connect(process.env.MONGO)
@@ -15,6 +18,11 @@ const PORT = 3000;
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
